998013f4db739b90a98ac3f1277ced33d0427608
Cal Monica Chen <monicachen808@gmail.com>
2023-06-01 14:15:40 -0400

    added navbar logo

--- src/Theme/MainTheme.js
+++ src/Theme/MainTheme.js
@@ -1,14 +1,17 @@
 import { createTheme } from '@mui/material/styles'
 
+const galaxyPurple = "#6a57b8"
+const galaxyBlue = "#2182c0"
+
 const MainTheme = createTheme({
     palette:{
         primary: {
-            main: "#6a57b8",
+            main: `${galaxyPurple}` ,
             light: "#76dbe1",
             dark: "#2182c0"
         },
         card:{
-            main:"#6a57b8",
+            main: `${galaxyBlue}`,
             light: "#a798e3",
             dark: "#373b81"
         }
--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -3,6 +3,10 @@ import { Link, Outlet} from 'react-router-dom'
 import { AppBar, Toolbar} from '@mui/material'
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Slide from '@mui/material/Slide';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/material'
+import { sizing } from "@mui/system"
+import logo from '../images/GalacticStopLogo.jpg'
 
 function HideOnScroll(props) {
     const { children } = props;
@@ -15,12 +19,16 @@ function HideOnScroll(props) {
       </Slide>
     );
   }
+
   
 
 function NavBar() {
+
+
   return (
     <>
-        <div>
+        {/*
+            <div>
             NavBar
             <ul>
             <li><Link to="/">home</Link></li>
@@ -28,16 +36,24 @@ function NavBar() {
             <li><Link to="/fuel">fuel</Link></li>
         </ul>
         </div>
+        */}
+        
         <HideOnScroll>
             <AppBar position="fixed">
-                <Toolbar>
-
-                    Galactic stop
+                <Toolbar disableGutters>
+                    <Box 
+                        component="img"
+                        sx={{height: "4em"}}
+                        alt="Galactic stop logo"
+                        src={logo}
+                    
+                    />
+                    
                 </Toolbar>
             </AppBar>
 
         </HideOnScroll>
-        
+        <Box sx={{height: "4em"}}/>
 
         <Outlet/>
     </>
