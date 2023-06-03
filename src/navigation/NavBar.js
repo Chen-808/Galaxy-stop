e790c09e3ae9cc4f4b8ccb1a89b6b4e2cf0272be
Cal Monica Chen <monicachen808@gmail.com>
2023-06-03 11:57:51 -0400

    modified navbar

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -3,10 +3,10 @@ import { Link, Outlet} from 'react-router-dom'
 import { AppBar, Toolbar} from '@mui/material'
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Slide from '@mui/material/Slide';
-import { Typography } from '@mui/material';
 import { Box } from '@mui/material'
-import { sizing } from "@mui/system"
 import logo from '../images/GalacticStopLogo.jpg'
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
 
 function HideOnScroll(props) {
     const { children } = props;
@@ -41,13 +41,28 @@ function NavBar() {
         <HideOnScroll>
             <AppBar position="fixed">
                 <Toolbar disableGutters>
-                    <Box 
+                    {/*
+                     <Box 
                         component="img"
-                        sx={{height: "4em"}}
+                        sx={{height: "4em", flexGrow: 1}}
                         alt="Galactic stop logo"
                         src={logo}
-                    
                     />
+                     */}
+                    <Box sx={{height: "4em", flexGrow: 1}}>
+                       <img 
+                        alt="Galactic stop logo"
+                        src={logo}
+                        width={350} 
+                        />
+                    </Box>
+                    
+                    <Tabs icon>
+                        <Tab label="Home" />
+                        <Tab label="Travelers" />
+                        <Tab label="Fuel" />
+                        <Tab label="Finds" />
+                    </Tabs>
                     
                 </Toolbar>
             </AppBar>
