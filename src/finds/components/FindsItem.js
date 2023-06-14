70945ce239dbd5a94cd9cb1dfda69bda012dc05e
Cal Monica Chen <monicachen808@gmail.com>
2023-06-14 16:34:48 -0400

    finished putting map on modal

--- src/finds/components/FindsItem.js
+++ src/finds/components/FindsItem.js
@@ -1,26 +1,14 @@
 import React from 'react'
+import "./MapStyles.css"
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom';
-import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
+import ModalMap from './ModalMap';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  //boxShadow: 24,
-  p: 4,
-  borderRadius: '5px'
-};
 
 function FindsItem(props) {
 
@@ -42,8 +30,9 @@ function FindsItem(props) {
         <CardContent>
           <Typography gutterBottom variant='h5'>{props.name}</Typography>
           <Typography gutterBottom variant='h6'>{props.description}</Typography>
-          <Typography gutterBottom variant='h6'>Finder: {props.creator}</Typography>
+          <Typography gutterBottom variant='h6'>Finder: {props.identifier}</Typography>
           <Typography gutterBottom variant='h6'>Address: {props.address}</Typography>
+          <Typography gutterBottom variant='h6'>coordinates: {props.coordinates.lat}</Typography>
         </CardContent>
         <CardActions>
             <Button size="small" onClick={handleOpen}>View on map</Button>
@@ -53,14 +42,7 @@ function FindsItem(props) {
               aria-labelledby="modal-modal-title"
               aria-describedby="modal-modal-description"
             >
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  {props.address}
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
-              </Box>
+              <ModalMap lat={props.coordinates.lat} lng={props.coordinates.lng} address={props.address}/>
             </Modal>
             <Button size="small">Edit</Button>
             <Button size="small">Delete</Button>
--- src/finds/components/ModalMap.js
+++ src/finds/components/ModalMap.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Box } from '@mui/material';
+import "./MapStyles.css"
+import "leaflet/dist/leaflet.css";
+import Typography from '@mui/material/Typography';
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
+import { Icon } from 'leaflet'
+import locationPin from '../../images/location-pin.png'
 
-function ModalMap() {
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  p: 4,
+  borderRadius: '5px'
+};
+
+function ModalMap(props) {
+    let lat = props.lat
+    let lng = props.lng
+    
+  const customIcon = new Icon({
+    iconUrl: locationPin,
+    iconSize: [38,38]
+  })
   return (
-    <div>ModalMap</div>
+    <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+            {props.address}
+        </Typography>
+        <MapContainer center={[lat, lng]} zoom={13}>
+            <TileLayer 
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[lat, lng]} icon={customIcon} />
+        </MapContainer>
+    </Box>
   )
 }
 
