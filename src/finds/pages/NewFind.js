f9f37407fb056f55ed8abf8620887e9078cfa2f1
Cal Monica Chen <monicachen808@gmail.com>
2023-07-04 14:18:29 -0400

    finished add new place form

--- src/finds/pages/NewFind.js
+++ src/finds/pages/NewFind.js
@@ -24,13 +24,54 @@ import { useForm } from 'react-hook-form'
       </Box>
 */
 function NewFind() {
+  const {register, handleSubmit, formState:{errors}} = useForm()
+  const onSubmit=(data) => console.log(data)
+
   return (
-      
-        
     <Container maxWidth="xs">
       <h1>New find</h1>
-      <TextField variant="outlined" label="Name" fullWidth/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box 
+          mb={2} 
+          sx={{
+          '& .MuiTextField-root': { m: 0.5 },
+          marginTop:4
+        }}>
+          <TextField 
+            variant="outlined" 
+            label="PlaceName" 
+            fullWidth
+            {...register("name", {required:"Name required"})}
+            error={!!errors?.name}
+            helperText={errors?.name ? errors.name.message : null}
+            
+            />
+          <TextField 
+            variant="outlined" 
+            label="Description" 
+            multiline
+            rows={4}
+            fullWidth
+            {...register("description", {required:"Description required"})}
+            error={!!errors?.description}
+            helperText={errors?.description ? errors.description.message : null}
+            />
 
+          <TextField 
+            variant="outlined" 
+            label="Address" 
+            fullWidth
+            {...register("address", {required:"Address required"})}
+            error={!!errors?.address}
+            helperText={errors?.address ? errors.address.message : null}
+            
+            />  
+        </Box>
+
+        <Button type="submit"variant="contained" color="primary" >
+          Add new find!
+        </Button>
+      </form>
       
 
 
