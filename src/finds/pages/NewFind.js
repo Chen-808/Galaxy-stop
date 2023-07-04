b333122d3eec75ed042ab2137b3de9139b95296a
Cal Monica Chen <monicachen808@gmail.com>
2023-07-04 13:36:45 -0400

    started form

--- src/finds/pages/NewFind.js
+++ src/finds/pages/NewFind.js
@@ -1,8 +1,43 @@
 import React from 'react'
-
+import { TextField, Box, Button, Container } from '@mui/material'
+import { useForm } from 'react-hook-form'
+ 
+/*
+<Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+          marginTop:4
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+          sx={{marginTop:4}}
+        />
+        </div>
+      </Box>
+*/
 function NewFind() {
   return (
-    <div>NewFind works yey</div>
+      
+        
+    <Container maxWidth="xs">
+      <h1>New find</h1>
+      <TextField variant="outlined" label="Name" fullWidth/>
+
+      
+
+
+    </Container>
+      
+      
+       
   )
 }
 
